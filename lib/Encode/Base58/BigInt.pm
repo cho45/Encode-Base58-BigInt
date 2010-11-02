56a3ac7ae5c540a582fa1ba7fc75a88a32d851da
cho45 <cho45@lowreal.net>
2010-11-02 19:06:19 +0900

    init

---
+++ lib/Encode/Base58/BigInt.pm
@@ -0,0 +1,36 @@
+package Encode::Base58::BigInt;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+
+1;
+__END__
+
+=head1 NAME
+
+Encode::Base58::BigInt - 
+
+=head1 SYNOPSIS
+
+  use Encode::Base58::BigInt;
+
+
+=head1 DESCRIPTION
+
+Encode::Base58::BigInt is 
+
+=head1 AUTHOR
+
+cho45 E<lt>cho45@lowreal.netE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
